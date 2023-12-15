26db849b20a65b0f95162ce0dc9bcdc1b69eb2c3
Raquel Universitária <131322647+rachelwitchburn@users.noreply.github.com>
2023-12-14 21:04:05 -0300

    Add files via upload

---
+++ boolean_tipo.py
@@ -0,0 +1,26 @@
+"""
+INICIAL MAIUSCULA SEMPRE
+True
+or
+False
+"""
+ativo = True
+print(ativo)
+
+"""
+OPERAÇÕES BÁSICAS
+"""
+
+# Negação (not) ->
+"""
+Fazendo a negação com o not, se bool for T, vai dar F. Se for F, vai dar T
+"""
+print(not ativo)
+logado = True
+#print(not True)
+#print(not False)
+
+#OU(OR)
+print(ativo or logado)
+#E(AND)
+print(ativo and logado)
