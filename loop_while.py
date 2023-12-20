ca4b5065ca2309f2df14fbf6889f088dd708bb27
Raquel Universitária <131322647+rachelwitchburn@users.noreply.github.com>
2023-12-20 15:50:27 -0300

    Add files via upload

---
+++ loop_while.py
@@ -0,0 +1,17 @@
+"""
+LOOP while
+"""
+
+"""
+EXEMPLO 1:
+numero = 1
+while numero <= 10:  # confere que é menor que 10
+    print(numero)  # printa ele
+    numero = numero + 1  # DEPOIS de printar, adiciona mais um e volta para o inicio do laço
+"""
+
+
+#  EXEMPLO 2:
+resposta = ''
+while resposta != 'sim':
+    resposta = input('PRONTO? ')
